1ff249a31db11c09ba68935ddcb35bd8965eb718
Ilmira <tregulovailmira@gmail.com>
2020-08-21 12:05:33 +0300

    Create object user (task 4)

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,22 @@ function logParenthis (number) {
 }
 
 console.log(logParenthis(5));
+
+/*4) Создать объект user'а. У объекта есть имя, дата рождения(Использовать Date в js),
+количество отработанных часов за месяц и ставка за час.
+     Реализовать геттеры (get age) на получение возраста и заработной платы.*/
+
+const user = {
+    name: 'Kate',
+    dateOfBirth: new Date(1995, 11, 10, 10, 30),
+    workedHours: 50,
+    perHourRate: 30,
+    get age() {
+        const currentDate = new Date();
+        let age = currentDate.getFullYear() - this.dateOfBirth.getFullYear();
+        return currentDate.setFullYear(2020) < this.dateOfBirth.setFullYear(2020) ? age - 1 : age;
+    },
+    get salary() {
+        return this.workedHours * this.perHourRate;
+    },
+}
