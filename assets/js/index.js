346267aab04cf85421b8f045f6e8f1b155280257
Ilmira <tregulovailmira@gmail.com>
2020-08-21 12:24:21 +0300

    Create object obj (task 6 from practice)

--- assets/js/index.js
+++ assets/js/index.js
@@ -136,3 +136,18 @@ function operation(num1, num2, fn) {
     return fn(num1, num2);
 };
 console.log(operation(number1, number2, getMathOperation));
+
+/*6) Создать объект obj, с методами method1(),method2() и method3(). В методе method3() должна
+возвращаться строка «метод3». Сделайте так, чтобы было возможно выполнение кода obj.method1().method2().method3()*/
+
+const obj = {
+    method1: function () {
+        return this;
+    },
+    method2: function () {
+        return this;
+    },
+    method3: function () {
+        return 'method3';
+    },
+};
